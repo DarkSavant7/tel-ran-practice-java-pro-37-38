c8fda89a5632431ccec58503a24cd8a0a7cfe0a2
Alex Grigorev <persian89g@gmail.com>
2023-01-18 16:35:49 +0300

    io

---
+++ src/main/java/de/telran/practice14/Animal.java
@@ -0,0 +1,10 @@
+package de.telran.practice14;
+
+import java.io.Serializable;
+
+public class Animal implements Serializable {
+
+    public Animal(String name) {
+        System.out.println("Animal born");
+    }
+}
---
+++ src/main/java/de/telran/practice15/Classwork.java
@@ -0,0 +1,91 @@
+package de.telran.practice15;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Classwork {
+
+    public static void main(String[] args) {
+//        filterUsingRegex();
+//        pattern();
+
+
+//        List<File> files = new ArrayList<>();
+//        searchFiles(new File("src"), files, ".java", 0);
+//        System.out.println("\nFinally: " + files);
+
+        List<File> files = searchFiles(new File("src"), ".java");
+        files.forEach(System.out::println);
+    }
+
+    private static void searchFiles(File root, List<File> list, String ext, int n) {
+        List<File> medium = Stream.of((Objects.requireNonNull(root.listFiles())))
+                .filter(f -> {
+                    if (f.isDirectory()) {
+                        searchFiles(f, list, ext, n + 1);
+                        return false;
+                    } else {
+                        return f.getName().endsWith(ext);
+                    }
+                })
+                .toList();
+        list.addAll(medium);
+
+        for (File fl : medium) {
+            System.out.println("localList -> " + fl.getAbsolutePath());
+        }
+        System.out.println("" + n + ":" + medium);
+    }
+
+    private static List<File> searchFiles(File root, String ext) {
+        List<File> files = new ArrayList<>();
+        List<File> medium = Stream.of((Objects.requireNonNull(root.listFiles())))
+                .filter(f -> {
+                    if (f.isDirectory()) {
+                        files.addAll(searchFiles(f, ext));
+                        return false;
+                    } else {
+                        return f.getName().endsWith(ext);
+                    }
+                })
+                .toList();
+        files.addAll(medium);
+        return files;
+    }
+
+    private static void pattern() {
+        String rawData = "AaaaaaBbbbbb11111CccccDddddd222EeeeeeFfff3333";
+//        String[] arr = rawData.split("\\d+");
+        Pattern pattern = Pattern.compile("([A-Z][a-z]+)([A-Z][a-z]+)(\\d+)");
+//        String[] arr = pattern.split(rawData);
+        String[] arr = pattern.split(rawData);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void filterUsingRegex() {
+        String rawData = "aaaaa 11111 jfjfgj kjdfgjndfsj fjfjfjf 2222 333333";
+        String[] rawArray = rawData.split("\\s");
+        List<String> words = new ArrayList<>();
+        List<Long> numbers = new ArrayList<>();
+        for (String element : rawArray) {
+            if (element.matches("\\d+")) {
+                numbers.add(Long.parseLong(element));
+            } else {
+                words.add(element);
+            }
+        }
+
+        System.out.println("Words: ");
+        System.out.println(words);
+        System.out.println("\nNumbers: ");
+        System.out.println(numbers);
+    }
+}
